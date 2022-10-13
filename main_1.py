cc3623d35043fbbf467dc8d51b3cfbc4aaee18f7
formicola-davide <formicola.davide@iisgalvanimi.edu.it>
2022-10-13 10:50:18 +0000

    Esercizio 1

---
+++ main_1.py
@@ -0,0 +1,25 @@
+x = 5 #creo una variabile x e le assegno un valore
+print(x) #stampo la variabile x
+y = 8 #creo una variabile y e le assegno un valore
+print(y)  #stampo la variabile y
+z = "paguro" #creo la variabile z e le assegno una parola
+print(z) #stampo la variabile z
+
+
+c = str(9) #creo una variabile c come una stringa
+d = int(7) #creo una variabile d come un numero intero
+e = float(2) #creo una variabile e come un numero decimale
+print(c)
+print(d)
+print(e)
+
+print(type(x)) #stampo la classe della variabile x
+r = 99 #creo la variabile r e le assegno un valore
+R = "mario" #creo una variabile R e le assegno una parola, R non sostituisce r
+print(r)
+print(R) 
+
+u ="hotel"
+print(u) #stampa entrambve le variabili
+u ='hotel' 
+print(u) #stampa entrambe le variabili
