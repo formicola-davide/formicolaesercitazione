dc56a67a2132c41c188a384c2347b7642d90c555
formicola-davide <formicola.davide@iisgalvanimi.edu.it>
2022-10-20 10:50:56 +0000

    versione 2

--- main_2.py
+++ main_2.py
@@ -1,11 +1,20 @@
 mylist = ["Lautaro", "Barella", "Bastoni"] #creo una lista con diverse parole
-print(thislist) #stampo la lista
+print(mylist)    #stampo la lista
 mylist = ["Lautaro", "Barella", "Bastoni", "Barella"] #posso mettere una parola più volte
 mylist = ["Lautaro", "Barella", "Bastoni"] 
-print(len(thislist)) #stampa il numero di parole nella lista
+print(len(mylist))    #stampa il numero di parole nella lista
 
 list1 = ["blu", "rosso","giallo"]
 list2 = ["cane","gatto", "gallo"] #posso creare diverse liste
 print(list1)
 print(list2) #posso stampare più liste
+
+list3 = ["bommo", "prema", "dobi"]
+print(type(list3)) #stampa il tipo di dati presenti nella lista
+
+list4 = ["ciao", 22, True, 40, "teaca"]  #nella lista posso mettere più elementi di classe diverse
+print(list4) 
+
+list5 = list(("correa", "dzeko", "bellanova")) #posso scrivere anche 2 parentesi
+print(list5)
 
