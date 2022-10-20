0586854fad46866fc25af26fefb406ac28f8dbe1
formicola-davide <formicola.davide@iisgalvanimi.edu.it>
2022-10-20 10:38:19 +0000

    versione 3

---
+++ main_2.py
@@ -0,0 +1,11 @@
+mylist = ["Lautaro", "Barella", "Bastoni"] #creo una lista con diverse parole
+print(thislist) #stampo la lista
+mylist = ["Lautaro", "Barella", "Bastoni", "Barella"] #posso mettere una parola più volte
+mylist = ["Lautaro", "Barella", "Bastoni"] 
+print(len(thislist)) #stampa il numero di parole nella lista
+
+list1 = ["blu", "rosso","giallo"]
+list2 = ["cane","gatto", "gallo"] #posso creare diverse liste
+print(list1)
+print(list2) #posso stampare più liste
+
---
+++ main_3.py
@@ -0,0 +1,44 @@
+x = 66 
+y = 45
+if x > y:  #impongo una condizione, ovvero se x è più grande di y
+    print("x è più grande di y") #stampare la frase, lasciare lo spazio prima di print
+
+a = 10
+b = 10
+if b > a: #impongo una condizione
+  print("b è mnaggiore di a")
+elif a == b: #scrivo elif ed impongo un altra condizione
+  print("b e a sono uguali") 
+
+c = 217
+d = 67
+if d > c:
+  print("d è maggiore di c")
+elif c == d:
+  print("c e d sono uguali")
+else:                         #impongo una terza condizione
+  print("c è maggiore di d")
+
+z= 200
+w = 33
+if z > w: print("z è maggiore di w") #scrivo tutto su una linea
+
+v = 800
+g = 69
+u = 402
+if v > g and u > v: #impongo più condizioni sulla stessa linea
+  print("entrambi le condizioni sono vere")
+
+A = 200
+B = 33 
+C = 500
+if A > B or A > C:   #impongo due condizioni, una con se ed uno con altrimenti
+  print("Almeno una condizione è vero")
+
+D = 50
+E = 218
+
+if E > D:
+  pass  #se la condizone è avverata passare
+
+
